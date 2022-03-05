9cb0d47d240cb83761cf89833ad4a4b3199f9ea0
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-03-05 07:43:22 +0200

    Added next best words to guess script

--- possible_words.py
+++ possible_words.py
@@ -40,7 +40,7 @@ def filter_words_by_excluded_letters(excluded_letters, word_list):
 
 def find_most_common_unknown_letters(known_letters, word_list):
     unknown_letters = []
-    unknown_letters_scores = {}
+    unknown_letters_scores = {} # {letter: score}
     for word in word_list:
         for letter in word:
             if letter in known_letters:
@@ -53,7 +53,18 @@ def find_most_common_unknown_letters(known_letters, word_list):
     return sorted(unknown_letters_scores.items(), key =
              lambda kv:(kv[1], kv[0]), reverse=True)
 
-# def find_best_words_to_guess(known_letters, excluded_letters, word_list):
+def find_best_words_to_guess(common_letters_dict, word_list):
+    word_scores = {} # {word: score}
+    for word in word_list:
+        score = 0
+        for letter in word:
+            # TODO refine to not reward words with repeated letters
+            if letter in common_letters_dict:
+                score += common_letters_dict[letter]
+        word_scores[word] = score
+    return sorted(word_scores.items(), key =
+             lambda kv:(kv[1], kv[0]), reverse=True)
+
 
 filtered_words = filter_words_by_known_letters(KNOWN_LETTERS, word_list)
 print("\nFILTERED BY KNOWN GREEN LETTERS:")
@@ -64,5 +75,9 @@ print(filtered_words)
 common_letters = find_most_common_unknown_letters(KNOWN_LETTERS, filtered_words)
 print_output = "\n".join(["{}: {}".format(letter, score) for letter, score in common_letters])
 print("\nMOST COMMON UNKNOWN LETTERS:")
+print(print_output)
+best_words = find_best_words_to_guess(dict(common_letters), filtered_words)
+print_output = "\n".join(["{}: {}".format(word, score) for word, score in best_words])
+print("\nBEST WORDS TO GUESS NEXT:")
 print(print_output)
 print("\n")
